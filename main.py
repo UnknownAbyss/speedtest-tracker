4aa8713f835a9b5b857d9c5efc388ce822ab3c14
UnknownAbyss <addityadd@gmail.com>
2020-08-02 21:53:46 +0530

    changes

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import json
 import matplotlib
-import tkinter
+import tkinter as tk
 import speedtest
+import os
 
+window = tk.Tk()
+window.geometry("200x100")
+
+
+Toggle = tk.Button(window,
+        text="Show Graph of speeds",
+        width=50,
+        command = showGraph)
+
+Filenames = os.listdir("./JSON")
+
+Toggle.pack()
+window.mainloop()
